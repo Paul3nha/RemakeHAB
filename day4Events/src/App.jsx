9b081432ee44e193c24429dd98efaaadaf2dfa4f
Paula Fernández <better_days1996@hotmail.com>
2024-09-30 09:07:13 +0200

    formulario completado

--- day4Events/src/App.jsx
+++ day4Events/src/App.jsx
@@ -1,21 +1,39 @@
 import "./App.css";
 
 function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Enviando o seu formulario...");
+  };
+
+  const handleChange = (e) => {
+    console.log("Día seleccionado:", e.target.value);
+  };
+
+  const handleClick = (e) => {
+    console.log(e);
+  };
+
   return (
     <>
-      <form onSubmit={}>
+      <form onSubmit={handleSubmit}>
         <h1>Proposta de novas actividades</h1>
-        <input type="text" placeholder="Actividade proposta" onChange={()=>} />
-        <select onChange={()=>}></select>
-        <option>--Escolla unha opción--</option>
-        <option>Luns</option>
-        <option>Martes</option>
-        <option>Mércores</option>
-        <option>Xoves</option>
-        <option>Venres</option>
-        <option>Sábado</option>
-        <option>Domingo</option>
-        <button onClick={}>Clíckame, non sexas tímida!</button>
+        <input
+          type="text"
+          placeholder="Actividade proposta"
+          onChange={(e) => console.log("on change:", e.target.value)}
+        />
+        <select onChange={handleChange}>
+          <option>--Escolla unha opción--</option>
+          <option>Luns</option>
+          <option>Martes</option>
+          <option>Mércores</option>
+          <option>Xoves</option>
+          <option>Venres</option>
+          <option>Sábado</option>
+          <option>Domingo</option>
+        </select>
+        <button onClick={handleClick}>Clíckame, non sexas tímida!</button>
       </form>
     </>
   );
