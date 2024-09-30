c0ac34293978fc1c615b6d994e6979cf0f51fd7b
Paula Fernández <better_days1996@hotmail.com>
2024-09-30 19:16:55 +0200

    aplicando cart context

--- day8Cart_DarkMode/src/CartContext.jsx
+++ day8Cart_DarkMode/src/CartContext.jsx
@@ -1,3 +1,26 @@
-export const CartContext = () => {
-  return <div>CartContext</div>;
+import { createContext, useContext, useState } from "react";
+import PropTypes from "prop-types";
+
+export const CartContext = createContext();
+
+export const useCart = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState(
+    JSON.parse(localStorage.getItem("cart")) || []
+  );
+
+  const enhancedSetCart = (better) => {
+    setCart(better);
+    localStorage.setItem("cart", JSON.stringify(better));
+  };
+  return (
+    <CartContext.Provider value={[cart, enhancedSetCart]}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+CartProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
