79654f91076a39ab3dda92cf94d95bdcb62852b2
Paula Fernández <better_days1996@hotmail.com>
2024-09-30 09:01:08 +0200

    dia 4 eventos: comenzamos diseñando el formulario que renderiza nuestra aplicación

---
+++ day4Events/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <form onSubmit={}>
+        <h1>Proposta de novas actividades</h1>
+        <input type="text" placeholder="Actividade proposta" onChange={()=>} />
+        <select onChange={()=>}></select>
+        <option>--Escolla unha opción--</option>
+        <option>Luns</option>
+        <option>Martes</option>
+        <option>Mércores</option>
+        <option>Xoves</option>
+        <option>Venres</option>
+        <option>Sábado</option>
+        <option>Domingo</option>
+        <button onClick={}>Clíckame, non sexas tímida!</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
