216127afbcfef283bbfab7ab3d7e126279b59c64
Paula Fernández <better_days1996@hotmail.com>
2024-10-01 13:36:24 +0200

    creación del del Dark Mode provider para cambiar a tema oscuro

--- day8Cart_DarkMode/src/DarkModeContext.jsx
+++ day8Cart_DarkMode/src/DarkModeContext.jsx
@@ -1,3 +1,18 @@
-export const DarkModeContext = () => {
-  return <div>DarkModeContext</div>;
+import { createContext, useContext, useState } from "react";
+import PropTypes from "prop-types";
+
+export const DarkModeContext = createContext();
+export const useDarkMode = () => useContext(DarkModeContext);
+
+export const DarkModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <DarkModeContext.Provider value={[darkMode, setDarkMode]}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+DarkModeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
--- day8Cart_DarkMode/src/main.jsx
+++ day8Cart_DarkMode/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./CartContext.jsx";
+import { DarkModeProvider } from "./DarkModeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <CartProvider>
-        <App />
+        <DarkModeProvider>
+          <App />
+        </DarkModeProvider>
       </CartProvider>
     </BrowserRouter>
   </StrictMode>
