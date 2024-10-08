b6af98c6e551248b18e8acdf84c67c8a03671cfa
Paula Fernández <better_days1996@hotmail.com>
2024-10-08 22:10:35 +0200

    falta corregir product.jsx

--- day8_PrimorFakeNew/src/routes/Product/Product.jsx
+++ day8_PrimorFakeNew/src/routes/Product/Product.jsx
@@ -3,10 +3,10 @@ import { useProduct } from "../../hooks/API.js";
 
 export const Product = () => {
   const { id } = useParams();
-  const { product, /*loading*/ error } = useProduct(id);
+  const { data: product, loading, error } = useProduct(id);
 
   // Manejo de loading, error o producto no encontrado
-  /*if (loading) return <p>Loading products...</p>;*/
+  if (loading) return <p>Loading products...</p>;
   if (error) return <p>Error fetching product</p>;
   if (!product) return <p>Product not found</p>;
 
