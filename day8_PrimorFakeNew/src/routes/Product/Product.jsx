90bf193a4e31a3942f919365e9dfb8e5a1de220f
Paula Fernández <better_days1996@hotmail.com>
2024-10-09 15:56:07 +0200

    añadimos botón de añadir al carrito

--- day8_PrimorFakeNew/src/routes/Product/Product.jsx
+++ day8_PrimorFakeNew/src/routes/Product/Product.jsx
@@ -1,14 +1,27 @@
 import { useParams } from "react-router-dom";
 import { useProduct } from "../../hooks/API.js";
-import "./Product.css";
 import { useDarkMode } from "../../DarkModeContext.jsx";
+import { useCart } from "../../CartContext.jsx";
+import { useState } from "react";
+import "./Product.css";
 
 export const Product = () => {
+  const [cart, setCart] = useCart();
   const [isDark] = useDarkMode();
   const { id } = useParams();
   const product = useProduct(id);
+  const [showMessage, setShowMessage] = useState(false);
 
   if (!product) return <p>Product not found</p>;
+
+  const handleCart = () => {
+    setCart([...cart, product]);
+    setShowMessage(true);
+
+    setTimeout(() => {
+      setShowMessage(false);
+    }, 3000);
+  };
 
   return (
     <section id="single-prod" className={isDark ? "dark" : "light"}>
@@ -18,6 +31,10 @@ export const Product = () => {
       <p className="more">Rating: {product.rating}</p>
       <p className="more">Stock: {product.stock}</p>
       <img className="more" src={product.thumbnail} alt={product.title} />
+      <button className="more" onClick={handleCart}>
+        🛒
+      </button>
+      {showMessage && <p className="more">Just Added to the cart!!</p>}
     </section>
   );
 };
