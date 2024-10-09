3362758522bcfd467202ba76a710cea529012008
Paula Fernández <better_days1996@hotmail.com>
2024-10-09 15:31:40 +0200

    arreglamos css añadiendo darkModeContext a Product.css

--- day8_PrimorFakeNew/src/routes/Product/Product.jsx
+++ day8_PrimorFakeNew/src/routes/Product/Product.jsx
@@ -1,20 +1,23 @@
 import { useParams } from "react-router-dom";
 import { useProduct } from "../../hooks/API.js";
+import "./Product.css";
+import { useDarkMode } from "../../DarkModeContext.jsx";
 
 export const Product = () => {
+  const [isDark] = useDarkMode();
   const { id } = useParams();
   const product = useProduct(id);
 
   if (!product) return <p>Product not found</p>;
 
   return (
-    <>
-      <h1>{product.title}</h1>
-      <p>{product.description}</p>
-      <p>Price: ${product.price}</p>
-      <p>Rating: {product.rating}</p>
-      <p>Stock: {product.stock}</p>
-      <img src={product.thumbnail} alt={product.title} />
-    </>
+    <section id="single-prod" className={isDark ? "dark" : "light"}>
+      <h2>{product.title}</h2>
+      <p className="more">{product.description}</p>
+      <p className="more">Price: ${product.price}</p>
+      <p className="more">Rating: {product.rating}</p>
+      <p className="more">Stock: {product.stock}</p>
+      <img className="more" src={product.thumbnail} alt={product.title} />
+    </section>
   );
 };
