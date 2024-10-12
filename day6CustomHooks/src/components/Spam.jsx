d05ad02553903f558d96c964bdda616982c7da59
Paula Fernández <better_days1996@hotmail.com>
2024-10-12 11:58:51 +0200

    implementando botón de cierre para el modal

--- day6CustomHooks/src/components/Spam.jsx
+++ day6CustomHooks/src/components/Spam.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 import { useDelay } from "../hooks/useDelay.js";
 import "./Spam.css";
 
 export const Spam = () => {
   const popUp = useDelay(5);
+  const [isOpen, setIsOpen] = useState(false); // Estado para controlar la visibilidad del modal
+
+  useEffect(() => {
+    if (popUp) {
+      setIsOpen(true); //abre el modal cuando popUp es verdadero
+    }
+  }, [popUp]);
+
+  const handleX = () => {
+    setIsOpen(false); //cerrará el modal
+  };
 
   return (
-    popUp && (
+    isOpen && ( // Solo renderiza si isOpen es verdadero
       <div className="modal-container">
         <div className="modal-content">
+          <button className="close-button" onClick={handleX}>
+            &times;
+          </button>
           <h1>Subscribe yourself to our services</h1>
         </div>
       </div>
