4af422edb5df0346e66615f1b424c2c2b00637fc
Paula Fernández <better_days1996@hotmail.com>
2024-10-13 20:42:06 +0200

    comenzando nuevo proyecto buscador

---
+++ day5BUSCADOR_MyRefToken/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components/Header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
